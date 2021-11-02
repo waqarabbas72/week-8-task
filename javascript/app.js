4b2a446aeed89bc77182fcee40617872ac61382b
waqarabbas72 <sheraliate751@gmail.com>
2021-11-02 16:02:18 +0500

    week-8

---
+++ javascript/app.js
@@ -0,0 +1,97 @@
+// let
+// numbers
+// let number = 12
+// console.log(typeof number)
+// let number = 13
+// console.log(typeof number)
+
+// let number = 13.5
+// console.log(typeof number)
+// let number = 1+2
+// console.log(typeof number)
+// let number = 1/2
+// console.log(typeof number)
+// let number = 1*2
+// console.log(typeof number)
+
+// strings
+// let name = "hello iam waqar abbas"
+// console.log(typeof name)
+
+// let name = "my father name is niat ali"
+// console.log(typeof name)
+// let name = "iam from nagar"
+// console.log(typeof name)
+// let name = "iam 18 years old"
+// console.log(typeof name)
+
+//boolean
+// let x = 5;
+// let y = 10;
+// let z = 6;
+// let less = x < y
+// console.log(less)
+// console.log(typeof less)
+
+// let array = [123];
+// console.log(typeof array)
+
+
+// operaters
+// let numberOne = 29;
+// let numberTwo = 41;
+// let numberThree = 70;
+
+// let addition = numberOne + numberThree;
+// console.log(addition)
+
+
+// let subtraction = numberThree - numberOne;
+// console.log(subtraction)
+
+// let multiplication = numberTwo * numberOne;
+// console.log(multiplication)
+
+// let division = numberTwo / numberOne;
+// console.log(division)
+
+// let remain = numberTwo % numberOne;
+// console.log(remain)
+
+// let square = numberTwo ** numberOne;
+// console.log(square)
+
+
+let name = "my name is Waqar Abbas";
+let nameU = name.toUpperCase()
+console.log(nameU)
+let nameS = name.toLowerCase()
+console.log(nameS)
+let newName = name.replace("Abbas", "Ali")
+console.log(newName)
+
+
+let fName = "my father name is Niat Ali";
+let fNameU = fName.toUpperCase()
+console.log(fNameU)
+let fNameS = fName.toLowerCase()
+console.log(fNameS)
+
+let age = "i am 18 years old";
+let ageU = age.toUpperCase()
+console.log(ageU)
+let ageS = age.toLowerCase()
+console.log(ageS)
+let newAge = age.replace("18", "19")
+console.log(newAge)
+ 
+let company = "North Aims Technologies";
+let companyU = company.toUpperCase()
+console.log(companyU)
+let companyS = company.toLowerCase()
+console.log(companyS)
+
+
+let text = "      hamid waqar    "
+let trim = text.trimStart()
+console.log(trim)
